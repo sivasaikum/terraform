6d857efc424af5d257def1de3149ffadee0697c0
sivasaikum <sivasaikumarallampati@gmail.com>
2025-02-23 12:23:43 +0530

    terraform

---
+++ aws-ec2/aws-ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "demo" {
+    ami             = "ami-09c813fb71547fc4f"
+    instance_type   = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+    tags = {
+        Name = "terraform-demo1"
+    }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+    name            = "allow_tls"
+    description     = "Allow TLS all inbound & outbound traffic"
+
+    ingress {
+        from_port = 22
+        to_port   = 22
+        protocol  = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_tls"
+    }
+}
